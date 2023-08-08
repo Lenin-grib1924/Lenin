743aa71b52bdceb6ce17f36ea221ade89587a694
lenin <lengribss@gmail.com>
2023-08-08 23:28:46 +0300

    Сохрнение изменений в 4 задаче

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,13 +3,14 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-int max = 0;
+
 Console.WriteLine("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
 if(numberA > max)
 {
     max = numberA;
