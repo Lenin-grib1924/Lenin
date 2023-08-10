3f261401d6c7187b3ae6ce337ac4ed8776044151
lenin <lengribss@gmail.com>
2023-08-10 21:40:09 +0300

    Добавлена задача № 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,4 +3,16 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = number.ToString().Length;
+if (length >= 3) {
+	while (number > 999)
+	{
+		number = number / 10;
+	}
+	int result = number % 10;
+	Console.WriteLine(result);
+} else {
+	Console.WriteLine("Третьей цифры нет");
+}
