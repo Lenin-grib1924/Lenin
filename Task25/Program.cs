d6f48a08fc80c432cc8f965103863c1b8c38ad9c
lenin <lengribss@gmail.com>
2023-09-04 23:14:34 +0300

    Сохранение домащней работы

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numbers (int A, int B)
+{
+    if (A == 0) return 0;
+    int res = 1;
+    for (int i = 0; i < B; i++)
+        res *= A;
+    return res;
+}
+
+  Console.Write("Введите число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+  int exp = numbers(A, B);
+  Console.WriteLine("Ответ: " + exp);
---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int number (int A)
+{
+    int res = 0;
+    while (A>0)
+    {
+        res += A%10;
+        A /= 10;
+    }
+    return res;
+}
+
+ Console.Write("Введите число: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+
+  int exp = number(A);
+  Console.WriteLine("Сумма цифр в числе: " + exp);
