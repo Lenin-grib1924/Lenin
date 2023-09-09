350543c6fbff925b2488204efddcb0136f28efbb
lenin <lengribss@gmail.com>
2023-09-09 18:25:10 +0300

    Сохранение

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -3,11 +3,21 @@
 
 int number  = new Random().Next(10, 100); // 99 + 1
 Console.WriteLine($"Случайное число в диапазоне 10 - 99 -> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
 // if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа - > {firstDigit}");
 // else Console.WriteLine($"Наибольшая цифра числа  -> {secondDigit}");
 
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа - > {result}");
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа - > {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа - > {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
