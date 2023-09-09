4d2b87cb68fb77485bc396feab0dbb15a1d724a9
lenin <lengribss@gmail.com>
2023-09-09 16:16:41 +0300

    Добавление новых задач

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
