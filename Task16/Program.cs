57cabf5b458befa8190c7915fa688fe9a0d31591
lenin <lengribss@gmail.com>
2023-09-09 23:56:45 +0300

    Сохранение задач

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+bool CheckSquareTwoNumbers(int firstNumber, int secondNumber)
+{
+    return firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber;
+}
+
+Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckSquareTwoNumbers(num1, num2);
+Console.WriteLine(result? "Да" : "Нет");
---
+++ Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (Х и У), причем Х не рано 0 и У не равно 0
+// и выдает номер четверти плоскости, в которой находится эта точка.
+
+int Quarter(int x, int y) // метод
+{
+    if(x > 0 && y > 0) return 1; // функция
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.WriteLine("Введите координаты");
+Console.Write("X: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+string result = quarter > 0 
+                ? $"Указанные координаты соответствуют четверти - > {quarter}"
+                : "Введены некорректные данные";
+Console.WriteLine(result);
+
+
