325ca99c58862ffcc7caf207ba3d99e4f171f51c
lenin <lengribss@gmail.com>
2023-09-09 18:13:14 +0300

    Сохранение

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+
+int number  = new Random().Next(10, 100); // 99 + 1
+Console.WriteLine($"Случайное число в диапазоне 10 - 99 -> {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа - > {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа  -> {secondDigit}");
+
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа - > {result}");
